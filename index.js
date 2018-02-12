839d6b1db6284c8b08734d8cd48936394fc91553
KaljakDev <victorrj2005@gmail.com>
2018-02-12 19:01:47 -0200

    Update index.js

--- index.js
+++ index.js
@@ -44,4 +44,4 @@ bot.on("message", async message => {
 });
 
 console.log(`Carregando o Codigo: <codigo secreto> ||| InvicttusBot`);
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
